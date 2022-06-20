831dfca85f6d2049139773e0923b33780e9c2682
Olya <ola.smolina@mail.ru>
2022-06-20 20:50:45 +0300

    Добавила задачу 3

---
+++ Homework_1_003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно без остатка).
+
+int number;
+string division = "нечётное";
+
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0) division = "чётное";
+
+Console.WriteLine($"Число {number} - это {division} число"); 
+
+
