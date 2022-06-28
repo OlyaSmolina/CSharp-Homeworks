e4273e5d971b017639439142b5b1c6297d4b6c22
Olya <ola.smolina@mail.ru>
2022-06-29 00:26:42 +0300

    Добавила задачу 1

---
+++ Homework_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int ShowSecondDigit()
+
+{
+    Console.WriteLine($"Input your three-digit number: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int dec = n % 100;
+    int ed = n % 10;
+    int number = (dec - ed)/10;
+    return number;
+}
+
+int result = ShowSecondDigit();
+Console.WriteLine($"The second digit of your number is {result}");
