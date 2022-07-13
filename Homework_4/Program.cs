46f5bbd90ca83076ea678f0a2608cb296ff2df69
Olya <ola.smolina@mail.ru>
2022-07-14 01:15:06 +0300

    Добавила решение второй задачи

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе
+
+// int FindSum(int n)
+// {
+//     int sum = 0;
+//     int current = Convert.ToString(n).Length;
+  
+//     for (int a = 0; a < current; a++)
+//     {
+//         sum = sum + (n - (n - (n % 10)));
+//         n = n / 10;
+//     }
+//     return sum;
+// }
+
+// Console.Write("Input your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Sum of numbers is " + FindSum(num));
+
+// Программа, которая задаёт массив из 8 чисел и выводит их на экран
+
+// void FillRandomArray(int[] numbers)
+// {
+//     int size = numbers.Length;
+//     for (int index = 0; index < size; index++)
+//     numbers[index] = new Random().Next(1, 8);  
+// }
+
+// void ShowRandomArray(int[] num)
+// {
+//     int length = num.Length;
+//     for (int i = 0; i < length; i++)
+//     System.Console.Write(num[i] + " ");
+// }
+// int[] array = new int [8];
+
+// FillRandomArray(array);
+// ShowRandomArray(array);
+
